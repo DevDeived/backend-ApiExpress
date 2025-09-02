f04ba0137657b09e4100643a5ae9e7c3d8ea3296
unknown <eliasneto6010@digitalcollege.internal>
2025-09-01 22:12:34 -0300

    TAREFA DE CASA, AJEITAR O UPDATE

--- repositories/categoriaRepository.js
+++ repositories/categoriaRepository.js
@@ -4,3 +4,40 @@ export const index = async() => {
     let results = await prisma.categoria.findMany()
     return results
 }
+
+export const find = async(id) => {
+    let results = await prisma.categoria.findUnique(
+        {
+            where: {id:Number(id)}
+        }
+    )
+    return results
+}
+
+export const create = async(nome) => {
+    let results = await prisma.categoria.create(
+        {
+            data: {nome}
+        }
+    )
+    return results
+}
+
+export const destroy = async(id) => {
+    let results = await prisma.categoria.delete(
+        {
+            where: {id:Number(id)}
+        }
+    )
+    return results
+}
+
+export const update = async(nome,id) => {
+    let results = await prisma.categoria.update(
+        {
+            where: {id:Number(id)},
+            data: {nome}
+        }
+    )
+    return results
+}
