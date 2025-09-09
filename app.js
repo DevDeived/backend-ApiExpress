e4141a3cd810c3fa348552de0eb94a7921d05125
Antonio Kauan Araujo Silva <antoniosilva0604@digitalcollege.internal>
2025-09-08 21:55:50 -0300

    feat/end_de_entrega

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser";
 import categoriaRoute from "./routes/categoriaRoute.js"
+import enderecoentregaRoute from "./routes/enderecoentregaRoutes.js"
 
 const app = express()
 const PORT = 3000
@@ -8,6 +9,7 @@ const PORT = 3000
 app.use(bodyParser.json());
 
 app.use("/api",categoriaRoute)
+app.use("/api",enderecoentregaRoute)
 
 app.listen(PORT, () => {
     console.log(`Aplicação rodando na porta ${PORT}`)
---
+++ prisma/migrations/20250909002107_/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `end_de_entrega` MODIFY `rua` VARCHAR(255) NOT NULL,
+    MODIFY `cep` VARCHAR(255) NOT NULL,
+    MODIFY `numero` VARCHAR(255) NOT NULL;
